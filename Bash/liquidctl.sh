d4e735ce0070255fa56666eeeea2d19609776000
Indoors-Cat <43878134+Indoors-Cat@users.noreply.github.com>
2022-06-13 21:44:31 -0400

    Will now prompt user for input on what theme to use

--- Bash/liquidctl.sh
+++ Bash/liquidctl.sh
@@ -1,30 +1,78 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-###Initilization:
-sudo liquidctl initialize all &
-sleep 2
+##Setting functions:
+function startliquidctl {
+    #Initialize liquidctl service:
+    sudo liquidctl initialize all --verbose &&
 
-###Static fan speeds:
-sudo liquidctl set fan1 speed 60 &
-sudo liquidctl set fan2 speed 60 &
-sudo liquidctl set fan3 speed 60 &
+    #Set fan speed to 60% duty cycle:
+    sudo liquidctl set fan1 speed 60 &&
+    sudo liquidctl set fan2 speed 60 &&
+    sudo liquidctl set fan3 speed 60 &&
+    return
+}
 
-###Temperature based fan speeds:
-##NOTE: This is based on a temperature probe and NOT CPU TEMP!!!
-#sudo liquidctl set fan1 speed 45 0 50 500 60 800 70 1100 80 1500 &
-#sudo liquidctl set fan2 speed 45 0 50 500 60 800 70 1100 80 1500 &
-#sudo liquidctl set fan3 speed 45 0 50 500 60 800 70 1100 80 1500 &
+function nordtheme {
+    sudo liquidctl set led1 color fixed 5E81AC --start-led 1 --maximum-leds 12 &&
+    sudo liquidctl set led2 color fixed 8FBCBB --start-led 1 --maximum-leds 10 &&
+    return
+}
 
-###Legend of Zelda Rupee Colors:(Green, Blue, Red, Gold[LED Strip1])
-#sudo liquidctl set led1 color color_pulse --speed medium 00ff00 00ff00 --start-led 1 --maximum-leds 4 &
-#sudo liquidctl set led1 color color_pulse --speed medium 0000ff 0000ff --start-led 5 --maximum-leds 4 &
-#sudo liquidctl set led1 color color_pulse --speed medium ff0000 ff0000 --start-led 9 --maximum-leds 4 &
-#sudo liquidctl set led2 color color_pulse --speed medium ffd700 ffd700 --start-led 1 --maximum-leds 10 &
+function draculatheme {
+    sudo liquidctl set led1 color fixed bd93f9 --start-led 1 --maximum-leds 12 &&
+    sudo liquidctl set led2 color fixed ff79c6 --start-led 1 --maximum-leds 10 &&
+    return
+}
 
-###Nord (Static?)
-sudo liquidctl set led1 color fixed 5E81AC --start-led 1 --maximum-leds 12 &
-sudo liquidctl set led2 color fixed 4C566A --start-led 1 --maximum-leds 10 &
+function greentheme {
+    sudo liquidctl set led1 color fixed 3FFF6E --start-led 1 --maximum-leds 12 &&
+    sudo liquidctl set led2 color fixed ff3e87 --start-led 1 --maximum-leds 12 &&
+    return
+}
 
-###Blue & Purple pulse: (Fans & LED Strip colors inverted)
-#sudo liquidctl set led1 color color_shift --speed medium 21BBB8 9c12f2 --start-led 1 --maximum-leds 48 &
-#sudo liquidctl set led2 color color_shift --speed medium 9c12f2 21BBB8 --start-led 1 --maximum-leds 48 &
+function rupeetheme {
+    sudo liquidctl set led1 color color_pulse --speed medium 00ff00 00ff00 --start-led 1 --maximum-leds 4 &
+    sudo liquidctl set led1 color color_pulse --speed medium 0000ff 0000ff --start-led 5 --maximum-leds 4 &
+    sudo liquidctl set led1 color color_pulse --speed medium ff0000 ff0000 --start-led 9 --maximum-leds 4 &
+    sudo liquidctl set led2 color color_pulse --speed medium ffd700 ffd700 --start-led 1 --maximum-leds 10 &
+    return
+}
+
+function clearliquid {
+    sudo liquidctl set led1 color off &&
+    sudo liquidctl set led2 color off &&
+    sudo liquidctl set led1 color clear &&
+    sudo liquidctl set led2 color clear &&
+    return
+}
+
+#Prompt user to start liquidctl:
+echo "Start liquidctl?"
+read liquid
+
+(if [ $liquid = "yes" ]; then
+    startliquidctl
+elif [ $liquid = "no" ]; then
+    exit 1
+else
+    echo "Error. Exiting now"
+fi)
+
+clearliquid
+
+#Prompt user for which theme to use:
+echo "Which theme should we use?"
+echo "nord, dracula, rupees, or green"
+read theme
+
+if [ $theme = "nord" ]; then
+    nordtheme
+elif [ $theme = "dracula" ]; then
+    draculatheme
+elif [ $theme = "green" ]; then
+    greentheme
+else
+    echo "Error. Exiting now"
+fi
+
+exit 0
